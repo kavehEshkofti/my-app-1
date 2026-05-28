e524398673eda61340af915ba2aad18386e53fe2
kavehEshkofti <kaveh_eshkofti@yahoo.com>
2026-05-28 16:37:32 +0200

    Add HelloWorld class with main method

---
+++ src/main/java/com/example.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("Hello World! Maven is working! 🚀");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("Current Time: " + new java.util.Date());
+        System.out.println("=================================");
+    }
+}
